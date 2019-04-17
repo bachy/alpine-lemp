0972cce227f876dc695c48e9ad490018792c4a84
Bachir Soussi Chiadmi <bachir@figureslibres.io>
2019-04-17 20:00:50 +0200

    fix

--- bin/bash.sh
+++ bin/bash.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
+echo -e '
+  _             _
+ | |__  __ _ __| |_
+ | `_ \/ _` (_-< ` \
+ |_.__/\__,_/__/_||_|
+'
+
 apk add bash bash-doc bash-completion
 
 sed -i 's/root:\/bin\/ash/root:\/bin\/bash/g' /etc/passwd
 
-exec bash
+exec bash && . install.sh
